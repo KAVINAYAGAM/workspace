d328c78074dc2a3c83df12e81241ee759464d847
neojarvis <techadmin@iamneo.ai>
2025-09-13 09:00:47 +0000

    Auto-save-commit-9/13/2025--2:30:46-PM

--- Project/src/test/java/utils/Base.java
+++ Project/src/test/java/utils/Base.java
@@ -1,127 +1,97 @@
-// package utils;
-
-// import java.io.FileInputStream;
-// import java.io.FileNotFoundException;
-// import java.io.IOException;
-// import java.net.MalformedURLException;
-// import java.net.URL;
-// import java.time.Duration;
-// import java.util.Properties;
-// import org.openqa.selenium.WebDriver;
-// import org.openqa.selenium.chrome.ChromeDriver;
-// import org.openqa.selenium.chrome.ChromeOptions;
-// import org.openqa.selenium.edge.EdgeDriver;
-// import org.openqa.selenium.firefox.FirefoxDriver;
-// import org.openqa.selenium.remote.RemoteWebDriver;
-// import org.openqa.selenium.support.events.EventFiringDecorator;
-// import org.openqa.selenium.support.events.WebDriverListener;
-
-// public class Base {
-
-//     public static WebDriver driver;
-//     public static FileInputStream file;
-//     public static Properties prop;
-
-//     public void loadProperties() throws IOException {
-//         String propertiesPath = System.getProperty("user.dir") + "/config/browser.properties";
-//         try {
-//             file = new FileInputStream(propertiesPath);
-//             prop = new Properties();
-//             prop.load(file);
-
-//         } catch (FileNotFoundException e) {
-//             e.printStackTrace();
-
-//         }
-//     }
-
-//     public void openBrowser() {
-
-//         try {
-//             loadProperties();
-//         } catch (IOException e) {
-//             // TODO Auto-generated catch block
-//             e.printStackTrace();
-//         }
-//         String executionType = prop.getProperty("executiontype");
-//         String browserName = prop.getProperty("browser");
-
-//         if ("remote".equalsIgnoreCase(executionType)) {
-//             URL gridUrl;
-//             try {
-//                 gridUrl = new URL(prop.getProperty("gridurl"));
-//                 driver = new RemoteWebDriver(gridUrl, new ChromeOptions());
-//             } catch (MalformedURLException e) {
-
-//                 e.printStackTrace();
-//             }
-
-//         } else if ("local".equalsIgnoreCase(executionType)) {
-//             switch (browserName.toLowerCase()) {
-//                 case "chrome":
-//                     driver = new ChromeDriver();
-//                     break;
-
-//                 case "edge":
-//                     driver = new EdgeDriver();
-//                     break;
-
-//                 case "firefox":
-//                     driver = new FirefoxDriver();
-//                     break;
-
-//                 default:
-//                     System.err.println("Unsupported browser: " + browserName);
-//                     break;
-//             }
-//         } else {
-//             System.err.println("Invalid execution type: " + executionType);
-//         }
-
-//         if (driver != null)
-
-//         {
-//             driver.manage().window().maximize();
-//             driver.get(prop.getProperty("url"));
-//             driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(8));
-//             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(8));
-
-//         }
-//         // Dont remove the listener Object
-
-//         WebDriverListener listener = new EventHandler();
-//         driver = new EventFiringDecorator<>(listener).decorate(driver);
-
-//     }
-
-// }
 package utils;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.remote.RemoteWebDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
-import org.apache.log4j.Logger;
-import org.apache.log4j.PropertyConfigurator;
+import java.time.Duration;
+import java.util.Properties;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.events.EventFiringDecorator;
+import org.openqa.selenium.support.events.WebDriverListener;
 
 public class Base {
+
     public static WebDriver driver;
-    public static Logger logger;
+    public static FileInputStream file;
+    public static Properties prop;
 
-    public static void setUp() throws Exception {
-        PropertyConfigurator.configure("src/main/resources/log4j.properties");
-        logger = Logger.getLogger(Base.class);
-        DesiredCapabilities caps = new DesiredCapabilities();
-        caps.setBrowserName("chrome");
-        driver = new RemoteWebDriver(new URL("http://localhost:4444"), caps);
-        logger.info("WebDriver Initialized (Remote Chrome).");
-        driver.manage().window().maximize();
-    }
+    public void loadProperties() throws IOException {
+        String propertiesPath = System.getProperty("user.dir") + "/config/browser.properties";
+        try {
+            file = new FileInputStream(propertiesPath);
+            prop = new Properties();
+            prop.load(file);
 
-    public static void tearDown() {
-        if (driver != null) {
-            driver.quit();
-            logger.info("WebDriver closed.");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+
         }
     }
+
+    public void openBrowser() {
+
+        try {
+            loadProperties();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        String executionType = prop.getProperty("executiontype");
+        String browserName = prop.getProperty("browser");
+
+        if ("remote".equalsIgnoreCase(executionType)) {
+            URL gridUrl;
+            try {
+                gridUrl = new URL(prop.getProperty("gridurl"));
+                driver = new RemoteWebDriver(gridUrl, new ChromeOptions());
+            } catch (MalformedURLException e) {
+
+                e.printStackTrace();
+            }
+
+        } else if ("local".equalsIgnoreCase(executionType)) {
+            switch (browserName.toLowerCase()) {
+                case "chrome":
+                    driver = new ChromeDriver();
+                    break;
+
+                case "edge":
+                    driver = new EdgeDriver();
+                    break;
+
+                case "firefox":
+                    driver = new FirefoxDriver();
+                    break;
+
+                default:
+                    System.err.println("Unsupported browser: " + browserName);
+                    break;
+            }
+        } else {
+            System.err.println("Invalid execution type: " + executionType);
+        }
+
+        if (driver != null)
+
+        {
+            driver.manage().window().maximize();
+            driver.get(prop.getProperty("url"));
+            driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(8));
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(8));
+
+        }
+        // Dont remove the listener Object
+
+        WebDriverListener listener = new EventHandler();
+        driver = new EventFiringDecorator<>(listener).decorate(driver);
+
+    }
+
 }
